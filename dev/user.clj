1e773529a0e75d95c991a666a95d4fad4878e15d
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2022-12-16 16:28:09 +0100

    Automatically reset system upon changes

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,8 @@
 (ns user
   (:require [clojure.java.browse :refer [browse-url]]
             [config.core :refer [env reload-env]]
+            [integrant-repl-autoreload.core :refer [start-auto-reset
+                                                    stop-auto-reset]]
             [integrant.repl :refer [go halt reset set-prep!]]
             [lijstje.core :refer [system-config]]
             [lijstje.domain :as domain]
@@ -13,7 +15,9 @@
   (go)
   (browse-url "http://localhost:3000/")
   (reset)
-  (halt))
+  (halt)
+  (start-auto-reset {:relevant-file? (constantly true)})
+  (stop-auto-reset))
 
 (comment
   (reload-env))
